ec2055336c26dbb0c75baba7e5ee3987b04bf3e1
testtube123 <32375907+testtube123@users.noreply.github.com>
2018-08-20 16:42:00 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+//#include "location.h"
 void bred () {                                                         // Color Functions 
   printf("\033[1;31m");
 }
 
-void bcyan () {
-
-  printf("\033[1;36m");                                                 
-
-}
 
 void red() {
 
   printf ("\033[0;31m");
 }
 
-void blue() {
-
-  printf ("\033[0;34m");
-
-}
-
-
-void bblue() {
-
-  printf ("\033[1;34m");
- }
-
 void bwhite () {
 
   printf ("\033[1;37m");
  }
 
-void white () {
-
-  printf ("\033[0;37m");
- }
 
 void reset () {     
   printf("\033[0m");
@@ -66,22 +46,46 @@ static int parseAndExecute()
         if (strcasecmp(verb, "quit") == 0)
           {
             clrscr ();
-             return 0;
-          }
+        	reset;     
+		return 0;
+  	}
         else if (strcasecmp(verb, "look") == 0)
           {
             printf("I can't see a thing.\n");
-          }
-        else if (strcasecmp(verb, "getup") == 0)
+	    
+	    //	    executeLook(noun);
+	  }
+        else if (strcasecmp(verb, "get") == 0)
           {
-            printf ("You decide against that knowing that you can't see anything.\n");
-          }
-        else
-          {
+            printf ("'%s' what?\n");
+	    //  executeget(noun);
+	  }
+	else if (strcasecmp(verb, "go") == 0)
+	  {  
+	   printf ("'%s' where?\n");
+	  }
+	else if (strcasecmp(verb, "help") ==0)
+	  {
+	   
+	    red (); 
+	    printf ("get/n");
+	    printf ("go/n");
+	    printf ("look/n");
+	    printf ("quit/n");
+	  }
+	    
+
+	  {
+	    printf ("'%s' Where");
+	      }
+	  {  
+	  else 
+	    {
             printf(" I don't know how to '%s'.\n", verb);
           }
       }
-    return 1;
+      
+	  return  1;
 }
 
 
@@ -106,9 +110,10 @@ int main ()
   {
     printf ("You wake up in a dark room on a cold steel bed. You don't remeber much of anything,and don't know how you got here.\n");
     printf ("You can't see a thing.\n");
-  while (getInput() && parseAndExecute());
-     printf("\nBye!\n");
-     clrscr ();
+  while (getInput() && parseAndExecute())
+    ;
+  //  executeLook("around");
+      clrscr ();
       return 0;
 
-}
+  }}
